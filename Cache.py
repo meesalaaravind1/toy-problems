ab0d35e7c5cfe02e2325d08dac61d19bd4748b50
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-04-14 00:38:16 +0530

    testcases

--- Cache.py
+++ Cache.py
@@ -5,10 +5,10 @@ class Cache:
         self.capacity = capacity
         self.files = {}     # key is name of
         self.lru = {}
-        self.count=0
 
     def put(self,key,data):
         pass
+
 
     def get(self,key):
         pass
@@ -16,3 +16,17 @@ class Cache:
     def get_cache(self):
         pass
 
+obj = Cache(5)
+
+assert obj.put(1,"krish") == "put success"
+assert obj.put(2,"aravind") == "put success"
+assert obj.put(3,"mourya") == "put success"
+assert obj.get(1) == "krish"
+assert obj.get(2) == "aravind"
+assert obj.get(2) == "aravind"
+assert obj.put(4,"meesala") == "put success"
+assert obj.put(5,"satish") == "put success"
+assert obj.put(6,"nikhil") == "put success"
+assert obj.put(7,"viswa") == "put success"
+assert obj.get_cache() == {1: 'krish', 2: 'aravind', 5: 'satish', 6: 'nikhil', 7: 'viswa'}
+print ("all test cases passed")
