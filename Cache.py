9c99176771429ce65cdfe06b6687d076760fa700
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-04-13 23:45:35 +0530

    no logic

--- Cache.py
+++ Cache.py
@@ -1,7 +1,18 @@
 class Cache:
-    def __init__(self,capacity,files,frequency):
+    def __init__(self,capacity):
         '''capacity is size,files is the file dictionary and frequency is the count of file opened.'''
         super().__init__()
         self.capacity = capacity
-        self.files = files
-        self.frequency = frequency
+        self.files = {}     # key is name of
+        self.lru = {}
+        self.count=0
+
+    def put(self,key,data):
+        pass
+
+    def get(self,key):
+        pass
+
+    def get_cache(self):
+        pass
+
