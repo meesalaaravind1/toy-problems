846d929af85878e491538a30c4b74abd6dad2f64
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-04-14 10:37:27 +0530

    testcases in seperate file

--- Cache.py
+++ Cache.py
@@ -25,17 +25,17 @@ class Cache:
     def get_cache(self):
         return self.files
 
-obj = Cache(5)
+# obj = Cache(5)
 
-assert obj.put(1,"krish") == "put success"
-assert obj.put(2,"aravind") == "put success"
-assert obj.put(3,"mourya") == "put success"
-assert obj.get(1) == "krish"
-assert obj.get(2) == "aravind"
-assert obj.get(2) == "aravind"
-assert obj.put(4,"meesala") == "put success"
-assert obj.put(5,"satish") == "put success"
-assert obj.put(6,"nikhil") == "put success"
-assert obj.put(7,"viswa") == "put success"
-assert obj.get_cache() == {1: 'krish', 2: 'aravind', 5: 'satish', 6: 'nikhil', 7: 'viswa'}
-print ("all test cases passed")
+# assert obj.put(1,"krish") == "put success"
+# assert obj.put(2,"aravind") == "put success"
+# assert obj.put(3,"mourya") == "put success"
+# assert obj.get(1) == "krish"
+# assert obj.get(2) == "aravind"
+# assert obj.get(2) == "aravind"
+# assert obj.put(4,"meesala") == "put success"
+# assert obj.put(5,"satish") == "put success"
+# assert obj.put(6,"nikhil") == "put success"
+# assert obj.put(7,"viswa") == "put success"
+# assert obj.get_cache() == {1: 'krish', 2: 'aravind', 5: 'satish', 6: 'nikhil', 7: 'viswa'}
+# print ("all test cases passed")
